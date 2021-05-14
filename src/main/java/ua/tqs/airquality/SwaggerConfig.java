825ca00c2d7b46abb392ae6a18361c5acdc344e4
Diogo Cunha <dgcunha@ua.pt>
2021-05-14 18:49:24 +0100

    swagger

---
+++ src/main/java/ua/tqs/airquality/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package ua.tqs.airquality;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.google.common.base.Predicate;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket postsApi() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("Documentation")
+            .apiInfo(apiInfo()).select().paths(postPaths()).build();
+    }
+
+    private Predicate<String> postPaths() {
+        return regex("/api.*");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("JavaInUse API")
+            .description("JavaInUse API reference for developers")
+            .termsOfServiceUrl("http://javainuse.com")
+            .license("JavaInUse License")
+            .licenseUrl("javainuse@gmail.com").version("1.0").build();
+    }
+
+}
